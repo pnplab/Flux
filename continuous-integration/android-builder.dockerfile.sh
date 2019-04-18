e0238f5bfb3010e9c4954082d6072a730a6faf59
Thibault Piront nuKs <gse.nuks@gmail.com>
2019-04-18 01:28:22 -0400

    refactor: move docker images to gitlab registry

--- continuous-integration/android-builder.dockerfile.sh
+++ continuous-integration/android-builder.dockerfile.sh
@@ -2,7 +2,13 @@
 
 set -ex
 
-echo "@warning make sure you've run docker login first!"
+echo "@warning make sure you've run \`docker login registry.gitlab.com\` first!"
 
 docker build -t pnplab/ci.gitlab.react-native-android -f android-builder.dockerfile .
-docker push pnplab/ci.gitlab.react-native-android:latest
+
+# Push onto dockerhub
+# docker push pnplab/ci.gitlab.react-native-android:latest
+
+# Push onto gitlab registry
+docker tag pnplab/ci.gitlab.react-native-android:latest registry.gitlab.com/pnplab/flux/android-builder:latest
+docker push registry.gitlab.com/pnplab/flux/android-builder:latest
