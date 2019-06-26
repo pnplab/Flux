9a40eab211e3f3cff1754a0b3b44b5d1bc5535b8
Thibault Piront nuKs <gse.nuks@gmail.com>
2019-06-26 01:13:00 -0400

    fix(*): prevent rsync script from syncing db

--- scripts/backend/rsync.sh
+++ scripts/backend/rsync.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -ex
 
-rsync -avzh --exclude-from=./backend/.gitignore ./backend/ root@pnplab.ca:~/aware-server
+# @warning the cd backend/ seems important as it seems `--exclude-from` will
+# take the current `pwd` to prepend the .gitignore file patterns.
+
+cd backend;
+rsync -avzh --exclude-from=.gitignore ./ root@pnplab.ca:~/aware-server
+cd ..
