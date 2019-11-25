a0ff418bd469a4d93c06c7044589ecb16b7dafd0
Thibault PIRONT <gse.nuks@gmail.com>
2019-11-25 13:22:31 -0500

    fix(restingstate): display video back and fill screen

--- crossplatform-components/RestingStateTask/RestingStateTaskVideoView.js
+++ crossplatform-components/RestingStateTask/RestingStateTaskVideoView.js
@@ -4,6 +4,7 @@
 
 import React from 'react';
 import { View, StatusBar } from 'react-native';
+import { Container, Content } from '../../crossplatform-theme';
 
 import styled from 'styled-components';
 import Video from 'react-native-video';
@@ -15,28 +16,32 @@ type Props = {
 };
 
 const RestingStateTaskVideoView = (props: Props) =>
-    <Wrapper>
+    <>
         <StatusBar hidden={true} />
-        <Video
-            source={require('../../assets/resting-state.mp4')}
-            volume={0.0} // 0 is muted, 1 is normal.
-            paused={false}
-            onLoad={props.onVideoStreamStarted} // Callback when playback starts
-            onEnd={props.onVideoStreamFinished} // Callback when playback finishes
-            onError={props.onVideoLoadingError} // Callback when video cannot be loaded
-            resizeMode="cover"
+        <Container>
+            <Video
+                source={require('../../assets/resting-state.mp4')}
+                volume={0.0} // 0 is muted, 1 is normal.
+                paused={false}
+                onLoad={props.onVideoStreamStarted} // Callback when playback starts
+                onEnd={props.onVideoStreamFinished} // Callback when playback finishes
+                onError={props.onVideoLoadingError} // Callback when video cannot be loaded
+                resizeMode="cover"
 
-            style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                bottom: 0,
-                right: 0
-            }}
-        />
-    </Wrapper>;
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    // Additional 20px for status bar.
+                    bottom: -20,
+                    right: 0
+                }}
+            />
+        </Container>
+    </>;
 
 const Wrapper = styled(View)`
+        position: relative;
         flex: 1;
     `;
 
