e9905578ad76637d46f536b7962aaa267422c0c9
Thibault PIRONT <gse.nuks@gmail.com>
2019-11-25 13:26:43 -0500

    style(theme): fix Container indent

--- crossplatform-theme/components/Container.js
+++ crossplatform-theme/components/Container.js
@@ -4,10 +4,10 @@ import variable from "./../variables/platform";
 
 const deviceHeight = Dimensions.get("window").height;
 export default (variables = variable) => {
-  const theme = {
-    flex: 1,
-    height: Platform.OS === "ios" ? deviceHeight : deviceHeight - 20
-  };
+    const theme = {
+        flex: 1,
+        height: Platform.OS === "ios" ? deviceHeight : deviceHeight - 20
+    };
 
-  return theme;
+    return theme;
 };
