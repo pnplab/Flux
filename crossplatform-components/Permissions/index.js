5c3b7fbfcc8729b1d2b24a6d98a2d6af1345f938
Thibault PIRONT <gse.nuks@gmail.com>
2019-12-24 03:05:49 -0500

    docs(permissions): write down random links

--- crossplatform-components/Permissions/index.js
+++ crossplatform-components/Permissions/index.js
@@ -3,3 +3,8 @@ export { default as RequestPermissions } from './RequestPermissionsController';
 export { default as RequestBypassDozePrivilege } from './RequestBypassDozePrivilegeController';
 export { default as RequestAccessibilityPrivilege } from './RequestAccessibilityPrivilegeController';
 export { default as RequestsConfirmation } from './RequestsConfirmationController';
+
+// A few interesting links.
+// https://developer.android.com/reference/android/Manifest.permission
+// https://stackoverflow.com/questions/28087032/android-get-application-account-outside-syncadapter-handle-sync-options
+// https://stackoverflow.com/a/32784499/939741
